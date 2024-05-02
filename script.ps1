881db55f130053934dcf810a2f924d625c63644f
Branislav Roljic <branislavroljic99@gmail.com>
2024-05-02 18:29:09 +0200

    #20 feat: Implement URLs and view for workspace management tasks

--- script.ps1
+++ script.ps1
@@ -4,9 +4,14 @@ pip install ".\api_soik\"
 
 pip install ".\simple_visualizer\"
 
-#pip install ".\graph_explorer\"
-
 pip install ".\block_visualizer\"
+
+pip install ".\xml_datasource\"
+
+pip install ".\json_datasource\"
+
+pip install ".\platform_soik\"
+
 # cd "graph_visualizer"
 python .\graph_explorer\src\graph_explorer\manage.py runserver
 
