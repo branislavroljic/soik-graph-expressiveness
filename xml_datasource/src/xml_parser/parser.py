54ba0620774a0b6c9e2bd419247594fe24f4bbd3
Nikola Jovic <jovnikola99@gmail.com>
2024-05-18 14:59:32 +0200

    #3 Fixing xml parser

--- xml_datasource/src/xml_parser/parser.py
+++ xml_datasource/src/xml_parser/parser.py
@@ -4,6 +4,7 @@ from api.models.node import Node
 from api.models.edge import Edge
 from api.services.loader import Parser
 
+
 class XMLParser(Parser):
     def __init__(self):
         self._nodes = {}
@@ -20,33 +21,86 @@ class XMLParser(Parser):
         try:
             tree = ET.parse(file)
             root = tree.getroot()
-            self._parse_nodes(root)
+            root_data = self._get_root(root)
+            self._parse_nodes(root_data)
             self._make_edges()
             self._graph.edges = set(self._graph.edges)
             self._graph.remove_duplicates_in_edges()
             return self._graph
+
         except FileNotFoundError:
             print("File does not exist")
             raise FileNotFoundError("XML file not found!")
 
-    def _parse_nodes(self, parent):
-        self._make_node(parent)
-        for child in parent:
-            self._parse_nodes(child)
+    def _get_root(self, data):
+        data.set('id', 'root')
+        self._add_ref_attr(data)
+        for child in list(data):
+            self._set_ref_to_parent(child, data)
+        return data
+
+    def _set_ref_to_parent(self, child, parent):
+        if child is not None:
+            if 'id' not in child.attrib:
+                child.set('id', 'value_' + str(self._counter))
+                self._counter += 1
+            if '@ref' in parent.attrib:
+                parent.attrib['@ref'] += ' ' + child.get('id')
+            else:
+                parent.attrib['@ref'] = child.get('id')
+
+    def _add_ref_attr(self, parent):
+        if parent is not None:
+            if '@ref' not in parent.attrib:
+                parent.attrib['@ref'] = ''
+
+    def _delete_attr(self, parent, attr_for_deleting):
+        for attribute in attr_for_deleting:
+            if attribute in parent.attrib:
+                del parent.attrib[attribute]
+        return parent
 
     def _make_node(self, parent):
-        node_data = {}
-        for key, value in parent.items():
-            node_data[key] = value
-        node = Node("value_" + str(self._counter), node_data)
-        self._counter += 1
-        self._graph.nodes.add(node)
-        self._nodes[node.id] = node
+        if 'id' not in parent.attrib:
+            parent.set('id', 'value_' + str(self._counter))
+            self._counter += 1
+        id_p = parent.get('id')
+        node = Node(id_p, self._element_to_dict(parent))
+        if node is not None:
+            self._graph.nodes.add(node)
+            self._nodes[id_p] = node
 
     def _make_edges(self):
-        for parent_id, parent_node in self._nodes.items():
-            for child in parent_node.data:
-                child_id = child["id"]
-                if child_id in self._nodes:
-                    edge = Edge(parent_node, self._nodes[child_id])
-                    self._graph.edges.add(edge)
+        for id, parent_node in self._nodes.items():
+            if '@ref' in parent_node.data.keys():
+                child_ids = parent_node.data['@ref'].split()
+                for child_id in child_ids:
+                    if child_id in self._nodes:
+                        child_node = self._nodes[child_id]
+                        edge = Edge(parent_node, child_node)
+                        self._graph.edges.add(edge)
+                del parent_node.data['@ref']
+
+    def _parse_nodes(self, parent):
+        attr_for_deleting = []
+        self._add_ref_attr(parent)
+        if parent is not None:
+            for key, value in parent.attrib.items():
+                if isinstance(value, str) and value.startswith('value_'):
+                    self._set_ref_to_parent(parent, parent)
+                    attr_for_deleting.append(key)
+
+            for child in list(parent):
+                self._set_ref_to_parent(child, parent)
+                self._make_node(child)  # Only create nodes for direct children
+
+            parent = self._delete_attr(parent, attr_for_deleting)
+            self._make_node(parent)
+
+    def _element_to_dict(self, element):
+        data = {}
+        for child in list(element):
+            data[child.tag] = child.text
+        data.update(element.attrib)
+        data['tag'] = element.tag
+        return data
